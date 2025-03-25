788b7215a622e4179871068e4c4123cef609bff5
Amir <amirkarami8304837@gmail.com>
2025-03-25 14:27:07 -0400

    created a single layer nural network using HE_initialzation

--- Animalclass.py
+++ Animalclass.py
@@ -1,59 +1,56 @@
 import numpy as np
 from lr_utils import load_dataset
 import h5py
-import time
 
-# implementing activation functions
-
-# sigmoind function
+train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
+print(train_set_x_orig.shape, train_set_y.shape, test_set_x_orig.shape, test_set_y.shape)
+train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
+test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
+train_set_x = train_set_x_flatten/255.
+test_set_x = test_set_x_flatten/255.
 def sigmoid(z):
     a = 1/ (1+ np.exp(-z))
     return a
-
-# RELU function
 def RELU(z):
     a = np.maximum(0, z)
     return a
-# tanh function
 def tanh(z):
     a = np.tanh(z)
     return a
-
-# parameter initialization method
-
-#zero method
-def initialize_zero_W(size_of_w):
-    w = np.zeros([size_of_w, 1])
-    return w
-def initialize_zero_b(size_of_b):
-    b = np.zeros([size_of_b])
-    
+def initialize_zero(dim, nodes, parameter):
+    if parameter == "w":
+        w = np.zeros([dim, nodes])
+        return w
+    b = np.zeros([nodes, dim])
     return b
-
-# random method
-def initialize_random(size_of_w):
-    w = np.random.rand(size_of_w,1)
+def initialize_random(dim, nodes):
+    w = np.random.rand(dim,nodes) * 0.00001
     print(w.shape)
     return w
-
-# he imethod
-#def initialize_he():
-
-def forward_propagation(W, X, b):
-    z = sigmoid(np.dot(W.T, X) + b)
-    return z
-
-def backward_propagation(A, Y, X):
+def HE_initialize(dim, nodes):
+    w = np.random.rand(dim, nodes) * np.sqrt(10/dim)
+    return w
+def forward_propagation(w1, w2, b1, b2, X):
+    A1 = tanh(np.dot(w1.T, X) + b1)
+    #print(A1, "\n\n")
+    A2 = sigmoid(np.dot(w2.T, A1) + b2)
+    #print(A2)
+    return A2, A1
+def backward_propagation(A2, A1, Y, X, W2):
     m = Y.shape[1]
-    dw = 1 / m * np.dot(X, (A - Y).T)
-    db = 1 / m * np.sum(A - Y)
-    return dw, db
+    dZ2 = A2 - Y
+    dW2 = (1/m) * np.dot(A1, dZ2.T)  # Shape (4, 1)
+    db2 = (1/m) * np.sum(dZ2, axis=1, keepdims=True) 
+    dZ1 = np.dot(W2, dZ2) * (1 - A1**2)
+    dW1 = (1/m) * np.dot(X, dZ1.T)  # Shape (12288, 4)
+    db1 = (1/m) * np.sum(dZ1, axis=1, keepdims=True)  # Shape (4, 1)
+    return dW1, db1, dW2, db2
+    
 def calculate_cost(Y, A):
     m = Y.shape[1]
-    cost = -1 / m * np.sum(Y * np.log(A) + (1 - Y) * np.log(1 - A))
+    epsilon = 1e-8  # Small constant to avoid log(0)
+    cost = -1 / m * np.sum(Y * np.log(A + epsilon) + (1 - Y) * np.log(1 - A + epsilon))
     return cost
-
-#prediction
 def perdiction(Z):
     for i in range(Z.shape[1]):
         if Z[0][i] > 0.5:
@@ -61,47 +58,39 @@ def perdiction(Z):
         else:
             Z[0][i] = 0
     return Z
-        
-# model function
-def model(X, Y, learning_rate):
-    W = initialize_zero_W(X.shape[0])
-    b = initialize_zero_b(1)
-    for i in range(4000):
-        A = forward_propagation(W, X, b)
-        dw, db = backward_propagation(A, Y, X)
-        W -= learning_rate * dw
-        b -= learning_rate * db
+def model(X, Y, learning_rate = 0.1):
+    w1 = HE_initialize(X.shape[0], 4)
+    b1 = initialize_zero(1,4,"b")
+    w2 = HE_initialize(4,1)
+    b2 = initialize_zero(1,1,"b")
+    for i in range(14000):
+        A2, A1 = forward_propagation(w1, w2, b1, b2, X)
+        #print(A)
+        dw1, dw2, db1, db2 = backward_propagation(A2, A1, Y, X, w2)
+        #print("dw1 = ", dw1,"\ndw2 = ", dw2,"\ndb1 = ", db1,"\ndb2 = ", db2)
+        w1 -= learning_rate * dw1
+        b1 -= learning_rate * db1
+        w2 -= learning_rate * dw2
+        b2 -= learning_rate * db2
         if i % 1000 == 0:
-            print(calculate_cost(Y, A))
+            print(calculate_cost(Y, A2))
     count = 0
-    # accuricy in train
-    Z = forward_propagation(W, X, b)
-    Z = perdiction(Z)
+    A2, A1 = forward_propagation(w1, w2, b1, b2, X)
     for i in range(Y.shape[1]):    
-        if Z[0][i] == Y[0][i]:
+        A2 = perdiction(A2)
+        if A2[0][i] == Y[0][i]:
             count += 1
     print(count/Y.shape[1] * 100)
-    return W, b
-# accuricy in test
-def test_deep(Y, X, W, b):
+    return w1, w2, b1, b2
+def test_deep(Y, X, w1, w2, b1, b2):
     count = 0
-    Z = forward_propagation(W, X, b)
-    Z = perdiction(Z)
+    Z, A1 = forward_propagation(w1, w2, b1, b2, X)
     for i in range(Y.shape[1]):
+        Z = perdiction(Z)
         if Z[0][i] == Y[0][i]:
             count += 1
     print(count/Y.shape[1] * 100)
 
-train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
-#print(train_set_x_orig.shape, train_set_y.shape, test_set_x_orig.shape, test_set_y.shape)
-train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
-test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
-train_set_x = train_set_x_flatten/255.
-test_set_x = test_set_x_flatten/255.
 
-W, b = model(train_set_x, train_set_y, 0.015)
-test_deep(test_set_y, test_set_x, W, b)
-
-
-
-
+w1, w2, b1, b2 = model(train_set_x, train_set_y)
+test_deep(test_set_y, test_set_x, w1, w2, b1, b2)
