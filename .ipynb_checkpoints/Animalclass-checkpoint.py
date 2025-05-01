017b43accacf0bd63a097a867f9e25e69372f6b3
Amir <amirkarami8304837@gmail.com>
2025-05-01 15:26:22 -0400

    added adam optimization with learning raete decay to the NN

--- .ipynb_checkpoints/Animalclass-checkpoint.py
+++ .ipynb_checkpoints/Animalclass-checkpoint.py
@@ -3,12 +3,14 @@ import matplotlib.pyplot as plt
 from lr_utils import load_dataset
 import h5py
 import time
+
 def shuffle_data(X, Y):
     m = X.shape[1]
     permutation = np.random.permutation(m)
     X_shuffled = X[:, permutation]
     Y_shuffled = Y[:, permutation]
     return X_shuffled, Y_shuffled
+
 def create_mini_batches(X, batch_size=64):
     m = X.shape[1]
     mini_batches = []
@@ -23,6 +25,8 @@ def create_mini_batches(X, batch_size=64):
         mini_batches.append(mini_batch)
 
     return mini_batches
+
+
 train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
 train_set_x = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
 test_set_x = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
@@ -34,15 +38,23 @@ train_x_mini = create_mini_batches(train_set_x_shuffled)
 train_y_mini = create_mini_batches(train_set_y_shuffeled)
 print(train_x_mini[0].shape)
 #print(train_x_mini[0][0])
+
 def sigmoid(z):
     a = 1/ (1+ np.exp(-z))
     return a
+
+def sigmoid(z):
+    a = 1/ (1+ np.exp(-z))
+    return a
+
 def RELU(z):
     a = np.maximum(0, z)
     return a
+
 def tanh(z):
     a = np.tanh(z)
     return a
+
 def initialize_zero(dimensions):
     layers = dimensions.shape[0]
     B = np.empty((layers, 1), dtype=object)
@@ -51,6 +63,7 @@ def initialize_zero(dimensions):
         b = np.zeros((dimensions[l][0], 1))
         B[l, 0] = b
     return B
+
 def initialize_random(dimensions):
     layers = dimensions.shape[0]
     W = np.empty((layers, 1), dtype=object)
@@ -58,6 +71,7 @@ def initialize_random(dimensions):
         w = np.random.rand(dimensions[l][1], dimensions[l][0])
         W[l,0] = w
     return W
+
 def HE_initialize(dimensions):
     layers = dimensions.shape[0]
     W = np.empty((layers, 1), dtype=object)
@@ -66,6 +80,7 @@ def HE_initialize(dimensions):
         w = np.random.rand(dimensions[l][1], dimensions[l][0])* np.sqrt(2 /dimensions[l][1])
         W[l,0] = w
     return W
+
 def forward_propagation(W, X, B):
     layers = W.shape[0]
     A = np.empty((layers+ 1, 1), dtype=object)
@@ -78,7 +93,8 @@ def forward_propagation(W, X, B):
             a = sigmoid(z)
         A[l+1,0] = a
     return A
-def backward_propagation(W, B, A, Y, use_L2 = False, lambd = 0.1):
+
+def backward_propagation(W, B, A, Y, use_L2 = False, lambd = None):
     m = Y.shape[1]
     L = W.shape[0] 
     dW = np.empty_like(W)
@@ -104,7 +120,8 @@ def backward_propagation(W, B, A, Y, use_L2 = False, lambd = 0.1):
         dB[l, 0] = np.sum(dZ, axis=1, keepdims=True) / m
 
     return dW, dB
-def calculate_cost(Y, A, use_L2 = False, lambd = 0.1):
+
+def calculate_cost(Y, A, use_L2 = False, lambd = None):
     m = Y.shape[1]
     cost = -1 / m * np.sum(Y * np.log(A) + (1 - Y) * np.log(1 - A))
     if use_L2 == True:
@@ -114,6 +131,7 @@ def calculate_cost(Y, A, use_L2 = False, lambd = 0.1):
         L2_cost *= lambd / (2 * m)
         return cost + L2_cost
     return cost
+
 def perdiction(Z):
     for i in range(Z.shape[1]):
         if Z[0][i] > 0.5:
@@ -121,6 +139,7 @@ def perdiction(Z):
         else:
             Z[0][i] = 0
     return Z
+        
 def get_layer():
     Layers = int(input("input number of layers: "))
     neurons = []
@@ -137,53 +156,27 @@ def get_layer():
     dim = np.array(dim)
     use_L2 = int(input("do you want to use L2 initialization: "))
     return Layers, dim, use_L2
-def Adam_optimization(W, B, Vw, Vb, Sw, Sb, dW, dB, iteration, learning_rate, B1=0.9, B2=0.999, epsilon=1e-8):
-    for l in range(W.shape[0]):
-        Vw[l, 0] = B1 * Vw[l, 0] + (1 - B1) * dW[l, 0]
-        Vb[l, 0] = B1 * Vb[l, 0] + (1 - B1) * dB[l, 0]
-        Sw[l, 0] = B2 * Sw[l, 0] + (1 - B2) * (dW[l, 0] ** 2)
-        Sb[l, 0] = B2 * Sb[l, 0] + (1 - B2) * (dB[l, 0] ** 2)
 
-        Vw_corrected = Vw[l, 0] / (1 - B1 ** iteration)
-        Vb_corrected = Vb[l, 0] / (1 - B1 ** iteration)
-        Sw_corrected = Sw[l, 0] / (1 - B2 ** iteration)
-        Sb_corrected = Sb[l, 0] / (1 - B2 ** iteration)
-
-        W[l, 0] -= learning_rate * (Vw_corrected / (np.sqrt(Sw_corrected) + epsilon))
-        B[l, 0] -= learning_rate * (Vb_corrected / (np.sqrt(Sb_corrected) + epsilon))
-
-    return Vw, Vb, Sw, Sb, W, B
-def model(X_mini, Y_mini, X, Y, learning_rate = 2, num_itiration = 10001, decay_rate = 0.001):
+def model(X_mini, Y_mini, X, Y, learning_rate = 0.005, num_itiration = 501):
     Layers, dim, use_L2 = get_layer()
     cost = []
     W = HE_initialize(dim)
     B = initialize_zero(dim)
-    Vw = np.empty_like(W)
-    Vb = np.empty_like(B)
-    Sw = np.empty_like(W)
-    Sb = np.empty_like(B)
-    for l in range(Vw.shape[0]):
-        Vw[l, 0] = np.zeros_like(W[l, 0])
-    for l in range(Vb.shape[0]):
-        Vb[l, 0] = np.zeros_like(B[l, 0])
-    for l in range(Sw.shape[0]):
-        Sw[l, 0] = np.zeros_like(W[l, 0])
-    for l in range(Sb.shape[0]):
-        Sb[l, 0] = np.zeros_like(B[l, 0])
     for i in range(num_itiration):
-        current_lr = learning_rate / (1 + decay_rate * i)
         for j in range(len(X_mini)):
             A = forward_propagation(W, X_mini[j], B)
             dW, dB = backward_propagation(W, B, A, Y_mini[j], use_L2)
-            vW, Vb, Sw, Sb, W, B = Adam_optimization(W, B, Vw, Vb, Sw, Sb, dW, dB, i * len(X_mini) + j + 1, current_lr)
+            W -= dW * learning_rate
+            B -= dB * learning_rate
         if i >= 50:
             A = forward_propagation(W, X, B)
             cost.append(calculate_cost(Y, A[-1][0], use_L2))
-            if i%200 ==0 or i == num_itiration - 1:
+            if i%1000 ==0 or i == num_itiration - 1:
                 print(cost[i -50])
     plt.plot(range(num_itiration- 50), cost) 
     plt.show()
     return W, B
+
 def test_deep(X, Y, W, B):
     count = 0
     A = forward_propagation(W, X, B)
@@ -194,9 +187,7 @@ def test_deep(X, Y, W, B):
     accuricy = count/Y.shape[1] * 100
     return accuricy
 
-    W, B = model(train_x_mini, train_y_mini, train_set_x_shuffled, train_set_y_shuffeled)
+W, B = model(train_x_mini, train_y_mini, train_set_x_shuffled, train_set_y_shuffeled)
 acc_test = test_deep(test_set_x, test_set_y, W, B)
 acc_train = test_deep(train_set_x, train_set_y, W, B)
 print(acc_train, "\n", acc_test)
-
-        
